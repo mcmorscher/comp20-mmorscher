6200fa88e08ce06cd59b0fa0268a7c0d7ec5f447
mcmorscher <24396166+mcmorscher@users.noreply.github.com>
2017-10-12 22:56:38 -0400

    Added map location functions

--- landmarks/load_map.js
+++ landmarks/load_map.js
@@ -2,7 +2,13 @@ var latitude, longitude;
 var googleMap;
 
 function findCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(function(curPosition) {
+        latitude = curPosition.coords.latitude;
+        longitude = curPosition.coords.longitude;
+    });
     
+    updateMapPosition();
+    addMarker(latitude, longitude, "My Location");
 }
 
 function loadMap() {
@@ -10,6 +16,18 @@ function loadMap() {
     googleMap = new google.maps.Map(document.getElementById("map"), {
         center: {lat: latitude, lng: longitude},
         zoom: 10
+    });
+}
+
+function updateMapPosition() {
+    googleMap.panTo(new google.maps.LatLng(latitude, longitude)); 
+}
+
+function addMarker(markLat, markLong, markTitle) {
+    new google.maps.Marker({
+        position: {lat: markLat, long: markLong},
+        map: googleMap, 
+        title: markTitle
     });
 }
 
