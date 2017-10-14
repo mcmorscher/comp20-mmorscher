71052e977c116e298dc4290b49ff91ac266ea1c2
mcmorscher <24396166+mcmorscher@users.noreply.github.com>
2017-10-14 16:24:38 -0400

    Fixed map functionality to display current location

--- landmarks/load_map.js
+++ landmarks/load_map.js
@@ -1,35 +1,40 @@
-
-var latitude, longitude;
-var googleMap;
+var latitude = 0, longitude = 0;
+var myCenter = new google.maps.LatLng(latitude, longitude);
 var username = "bBcpK9Na";
+var googleMap;
 
 function findCurrentLocation() {
     navigator.geolocation.getCurrentPosition(function(curPosition) {
         latitude = curPosition.coords.latitude;
         longitude = curPosition.coords.longitude;
+        
+        myCenter = new google.maps.LatLng(latitude, longitude);
+        googleMap.panTo(myCenter);
+        
+        addMarker(myCenter, username);
     });
-    
-    //updateMapPosition();
-    //addMarker(latitude, longitude, "My Location");
 }
 
 function loadMap() {
-    findCurrentLocation();
     googleMap = new google.maps.Map(document.getElementById("map"), {
-        center: new google.maps.LatLng(latitude, longitude),
-        zoom: 10
+    center: myCenter,
+    zoom: 12
     });
+
+    findCurrentLocation();
 }
 
-function updateMapPosition() {
-    googleMap.panTo(new google.maps.LatLng(latitude, longitude)); 
-}
-
-function addMarker(markLat, markLong, markTitle) {
-    marker = new google.maps.Marker({
-        position: {lat: markLat, long: markLong},
+function addMarker(markPos, markTitle) {
+    mark = new google.maps.Marker({
+        position: markPos,
         map: googleMap, 
         title: markTitle
     });
+    
+    google.maps.event.addListener(mark, "click", function() {
+        popup = new google.maps.InfoWindow();
+		popup.setContent(markTitle + "'s Location");
+		popup.open(map, mark);
+	});
 }
 
