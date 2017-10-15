bc57bdfa58686f054791c0d62c1c72128438c40a
mcmorscher <24396166+mcmorscher@users.noreply.github.com>
2017-10-15 16:30:13 -0400

    Added functions to parse JSON response and place other people's locations on the map

--- landmarks/load_map.js
+++ landmarks/load_map.js
@@ -42,21 +42,37 @@ function addMarker(markPos, markTitle, iconType) {
     google.maps.event.addListener(mark, "click", function() {
         popup = new google.maps.InfoWindow();
 		popup.setContent(markTitle + "'s Location");
-		popup.open(map, mark);
+		popup.open(map, this);
 	});
 }
 
 function getOtherLocations(curLat, curLong) {        
-        var result;
+        var result, response;
         var request = new XMLHttpRequest();
         
         request.open("POST", "https://defense-in-derpth.herokuapp.com/sendLocation", true);
         request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         request.onreadystatechange = function() {
             if(request.readyState == 4 && request.status == 200) {
-                //console.log(request.responseText);   
+                response = JSON.parse(request.responseText);
+                showOtherPeople(response.people);
             }
         }
         result = "login=" + username + "&lat=" + curLat + "&lng=" + curLong;
         request.send(result);
+}
+
+function showOtherPeople(people) {
+    for (person of people) {
+        position = new google.maps.LatLng(person.lat, person.lng);
+        
+        //If my username is returned from datastore, still display my unique icon
+        if(person.login == username) {
+            addMarker(position, person.login, "me");
+        }
+        //For other users, display the icon for others
+        else {
+            addMarker(position, person.login, "other");
+        }
+    }
 }
